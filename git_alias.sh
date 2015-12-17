d7f9e998adffff3747645f637500577e694e11ac
vra <wangyf11@mail.ustc.edu.cn>
2015-12-17 11:58:33 +0800

    add git alias file

---
+++ git_alias.sh
@@ -0,0 +1,11 @@
+git config --global alias.st status
+git config --global alias.a add  
+git config --global alias.p push 
+git config --global alias.pu pull
+git config --global alias.ci commit
+git config --global alias.br branch
+git config --global alias.unstage 'reset HEAD'
+git config --global alias.last 'log -1'
+git config --global alias.co checkout
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+
